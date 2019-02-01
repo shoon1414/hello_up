a90d0477e1e7fa10c91b8e410966b51d41be0f9e
csuser <csuser@ShoOns-MacBook-Pro.local>
2019-02-01 16:42:47 +0900

    hello_up追加

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def hello
+    render html: "¡hello, world!!"
+  end
+  def bye
+    render html: "goodbye, world!"
+  end
+end
